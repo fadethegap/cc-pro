68534c574954acd9d38d6c3776afe0854812a6c2
John Kovach <john@kovachenterprises.com>
2021-12-02 17:26:41 -0500

    moved assets folder to src

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/assets/`,
+        path: `${__dirname}/src/assets/`,
       },
     },
     "gatsby-plugin-sharp",
